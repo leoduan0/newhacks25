790ad88719de71a7b3fb38e925de0ac6a8e990b5
Thomas Zhang <69397338+ThomasZhang223@users.noreply.github.com>
2025-10-25 19:43:31 -0400

    j

--- backend/app.py
+++ backend/app.py
@@ -48,4 +48,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=True)
