0bd7d9caf35fc07333cf9cc8f72ef4cf29d307eb
Milky-Way-Cookie <lianso125@gmail.com>
2025-10-25 14:38:49 -0400

    Updated TransactionDetail and Log pages with new layout and styles

---
+++ app/app/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Log from './(tabs)/log';
+import TransactionDetail from './(tabs)/transactiondetails';
+
+export type RootStackParamList = {
+  Log: undefined;
+  TransactionDetail: { transaction: any }; // could type this more strictly
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Log">
+        <Stack.Screen
+          name="Log"
+          component={Log}
+          options={{ title: 'Transactions' }}
+        />
+        <Stack.Screen
+          name="TransactionDetail"
+          component={TransactionDetail}
+          options={{ title: 'Transaction Details' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Log from './log';
+// import TransactionDetail from './transactiondetails';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Log">
+//         <Stack.Screen name="Log" component={Log} options={{ title: 'Transactions' }} />
+//         <Stack.Screen name="TransactionDetail" component={TransactionDetail} options={{ title: 'Transaction Details' }} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
