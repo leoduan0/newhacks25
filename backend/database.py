71d65f03c0018cf6039ddd9e49c3f6aade3ca930
lucas-user <lucasth.chan@mail.utoronto.ca>
2025-10-25 20:30:15 -0400

    database json

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,7 @@ supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_
 
 def load_from_json(json_text: str):
     formatted = json.loads(json_text)
-    insert_receipt(
+    insert_data(
         merchant_name=formatted["merchant_name"],
         items=formatted["items"],
         receipt_type=formatted["type"],
@@ -20,9 +20,9 @@ def load_from_json(json_text: str):
     )
 
 
-def get_id():
+def get_id(table_name: str):
     new_id = uuid.uuid1()
-    response = supabase.table("response").select("id").eq("id", new_id).execute()
+    response = supabase.table(table_name).select("id").eq("id", new_id).execute()
 
     if len(response.data) > 0:
         new_id = uuid.uuid1()
@@ -30,19 +30,24 @@ def get_id():
     return new_id
 
 
-def switch_format(receipt_type: str, merchant_name: str, items: list[tuple[str, float]], total_amount: float, purchase_date=None) -> dict:
-    item_list = []
+def insert_data(receipt_type: str, merchant_name: str, items: list, total_amount: float, purchase_date=None) -> dict:
+    receipt_id = get_id("records")
+
     for item in items:
+        item_id = get_id("items")
         item_dict = {
-            "name": item[0],
-            "cost": item[1],
+            "id": item_id,
+            "name": item["name"],
+            "cost": item["price"],
             "created_at": str(datetime.now().isoformat()),
             "updated_at": str(datetime.now().isoformat()),
             "userId": "dc5b034a-d418-4e3f-8069-e7bb21550870",
         }
-        item_list += item_dict
+        supabase.table("items").insert(item_dict).execute()
+        supabase.table("_TransactionItems").insert({'A': item_id, 'B': receipt_id}).execute()
 
-    formatted = {
+    records = {
+        "id": receipt_id,
         "store": merchant_name,
         "category": receipt_type,
         "imageUrl": "",
@@ -52,37 +57,8 @@ def switch_format(receipt_type: str, merchant_name: str, items: list[tuple[str, 
         "createdAt": str(datetime.now().isoformat()),
         "updatedAt": str(datetime.now().isoformat()),
         "userId": "dc5b034a-d418-4e3f-8069-e7bb21550870",
-        "items": item_list
     }
-
-    return formatted
-
-
-def insert_receipt(receipt_type: str, merchant_name: str, items: list[tuple[str, float]], total_amount: float, purchase_date=None):
-    item_list = []
-    for item in items:
-        item_dict = {
-            "name": item[0],
-            "cost": item[1],
-            "createdAt": str(datetime.now().isoformat()),
-            "updatedAt": str(datetime.now().isoformat()),
-            "userId": "dc5b034a-d418-4e3f-8069-e7bb21550870",
-        }
-        item_list += item_dict
-
-    formatted = {
-        "store": merchant_name,
-        "category": receipt_type,
-        "imageUrl": "",
-        "address": "",
-        "phone": "",
-        "notes": "",
-        "createdAt": str(datetime.now().isoformat()),
-        "updatedAt": str(datetime.now().isoformat()),
-        "userId": "dc5b034a-d418-4e3f-8069-e7bb21550870",
-        "items": item_list
-    }
-
+    supabase.table("records").insert(records).execute()
 
 
 def get_row_by_id(row_id: str):
@@ -98,6 +74,7 @@ def get_row_by_id(row_id: str):
     except Exception as e:
         print(f"Error retrieving data: {e}")
         return None
+
 
 def get_all_rows():
     return supabase.table("records").select("*").execute()
